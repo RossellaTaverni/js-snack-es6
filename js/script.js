c264b946030052e910e34f941476646218f4bdb9
Rossella <rossellataverni02@gmail.com>
2025-03-28 16:16:37 +0100

    [UPD] Snack 2 Update Start.

--- js/script.js
+++ js/script.js
@@ -23,3 +23,10 @@ for (let i = 1; i < biciDaCorsa.length; i++){
 
 // 4. Stampiamo in console la bici con il peso minore.
 console.log('Bici con il peso minore:', biciPesoMinore);
+
+
+/* Snack2 Creare un array di oggetti di squadre di calcio. 
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. 
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. 
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. 
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
