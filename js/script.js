eb2100220a4cc5602bf5fe565d2637c029703e10
Rossella <rossellataverni02@gmail.com>
2025-03-28 15:54:07 +0100

    [UPD] Snack 1 update, array creation.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* Snack 1 Creare un array di oggetti: 
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
+Stampare in console la bici con peso minore. */
+
+// 1. Creiamo l'array di oggetti
+let biciDaCorsa = [
+    { nome: 'Giant TCR Advanced SL 0', peso: 6.4 },
+    { nome: 'Scott Addict RC Ultimate', peso: 5.9 },
+    { nome: 'Specialized S-Works Aethos/Sram Red Etap AXS', peso: 6.34 },
+    { nome: 'Wilier Triestina Verticale SLR', peso: 6.5 },
+    { nome: 'Orbea Orca M10iLTD Replica', peso: 6.7 }
+]
