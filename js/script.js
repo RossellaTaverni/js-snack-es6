23f9f6d834950df0ab367bcaba8ae36f9ae8f227
Rossella <rossellataverni02@gmail.com>
2025-03-28 16:14:25 +0100

    [UPD] First complete exercise.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 Stampare in console la bici con peso minore. */
 
-// 1. Creiamo l'array di oggetti
+// 1. Creiamo l'array di oggetti.
 let biciDaCorsa = [
     { nome: 'Giant TCR Advanced SL 0', peso: 6.4 },
     { nome: 'Scott Addict RC Ultimate', peso: 5.9 },
@@ -10,3 +10,16 @@ let biciDaCorsa = [
     { nome: 'Wilier Triestina Verticale SLR', peso: 6.5 },
     { nome: 'Orbea Orca M10iLTD Replica', peso: 6.7 }
 ]
+
+// 2. Inizializziamo una variabile con il primo ggetto dell'array: biciDaCorsa.
+let biciPesoMinore = biciDaCorsa[0];
+
+// 3. Creiamo un ciclo per scorrere l'array e confrontare i pesi.
+for (let i = 1; i < biciDaCorsa.length; i++){
+    if(biciDaCorsa[i].peso < biciPesoMinore.peso){
+        biciPesoMinore = biciDaCorsa[i];
+    }
+}
+
+// 4. Stampiamo in console la bici con il peso minore.
+console.log('Bici con il peso minore:', biciPesoMinore);
